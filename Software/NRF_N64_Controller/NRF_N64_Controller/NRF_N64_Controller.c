9c59ba513a229862b0204f2f2d8470137f6cfbff
Robotto <mark@moore.dk>
2012-10-10 10:05:17 +0200

    added AtmelStudio6 project for controller

---
+++ Software/NRF_N64_Controller/NRF_N64_Controller/NRF_N64_Controller.c
@@ -0,0 +1,17 @@
+/*
+ * NRF_N64_Controller.c
+ *
+ * Created: 10-10-2012 10:04:01
+ *  Author: Robotto
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
