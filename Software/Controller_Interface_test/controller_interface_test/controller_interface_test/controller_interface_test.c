217e77f4c8df935f5b2ce2c3874d2673004f7d1b
nicedude <kbmjensen@gmail.com>
2013-07-02 22:17:01 +0200

    added new project to test nintendo controller interface

---
+++ Software/Controller_Interface_test/controller_interface_test/controller_interface_test/controller_interface_test.c
@@ -0,0 +1,17 @@
+/*
+ * controller_interface_test.c
+ *
+ * Created: 7/2/2013 22:01:56
+ *  Author: delfi
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
